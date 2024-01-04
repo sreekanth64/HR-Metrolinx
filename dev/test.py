f10a9606a33ae72585fb67050754158ef6ecdf71
sreekanth64 <sreekanth64>
2024-01-04 10:57:07 +0000

    done

--- dev/test.py
+++ dev/test.py
@@ -1,2 +1,39 @@
 # Databricks notebook source
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
 
+# Create SparkSession
+spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
+
+# Prepare Data
+data = [("James", "Sales", 3000), \
+    ("Michael", "Sales", 4600), \
+    ("Robert", "Sales", 4100), \
+    ("Maria", "Finance", 3000), \
+    ("James", "Sales", 3000), \
+    ("Scott", "Finance", 3300), \
+    ("Jen", "Finance", 3900), \
+    ("Jeff", "Marketing", 3000), \
+    ("Kumar", "Marketing", 2000), \
+    ("Saif", "Sales", 4100) \
+  ]
+
+# Create DataFrame
+columns= ["employee_name", "department", "salary"]
+df = spark.createDataFrame(data = data, schema = columns)
+df.printSchema()
+df.show(truncate=False)
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+distinctDF = df.distinct()
+print("Distinct count: "+str(distinctDF.count()))
+distinctDF.show(truncate=False)
+
+# COMMAND ----------
+
+display(distinctDF)
